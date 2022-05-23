20c57cfe05c3ea4b00717fb54ae3568f00417bcb
Ahmed Khater <s294633@studenti.polito.it>
2022-05-23 09:28:45 +0200

    add: Unit tests for DAO classes: Item.test.js, user.test.js, testResult.test.js

--- code/server/classes/DAOs/ItemDAO.js
+++ code/server/classes/DAOs/ItemDAO.js
@@ -1,118 +1,140 @@
 const sqlite = require('sqlite3');
 const Item = require('../Item');
 
-class ItemDAO{
+class ItemDAO {
+	constructor(db) {
+		this.db = db;
+	}
 
-    constructor(db){
-        this.db = db;
-    }
+	addItem(item) {
+		return new Promise((resolve, reject) => {
+			const sql =
+				'INSERT INTO ITEM_TABLE(DESCRIPTION, PRICE, USERID, SKUID) VALUES(?,?,?,?)';
+			this.db.run(
+				sql,
+				[
+					item.getDescription(),
+					item.getPrice(),
+					item.getSupplierId(),
+					item.getSKUId(),
+				],
+				(err) => {
+					if (err) {
+						reject(err);
+					} else {
+						resolve(201);
+					}
+				}
+			);
+		});
+	}
 
-    addItem(item) {
-        return new Promise((resolve, reject) => {
-            const sql = "INSERT INTO ITEM_TABLE(DESCRIPTION, PRICE, USERID, SKUID) VALUES(?,?,?,?)";
-            this.db.run(sql, [item.getDescription(), item.getPrice(), item.getSupplierId(), item.getSKUId()], err => {
-                if(err){
-                    reject(err);
-                }else{
-                    resolve(201);
-                }
-            });
-        });
-    }
+	getItemById(id) {
+		return new Promise((resolve, reject) => {
+			const sql = 'SELECT * FROM ITEM_TABLE WHERE ID = ?';
+			this.db.get(sql, [id], (err, row) => {
+				if (err) {
+					reject(err);
+					return;
+				}
+				if (row === undefined) {
+					resolve(undefined);
+				} else {
+					resolve(
+						new Item(row.DESCRIPTION, row.PRICE, row.USERID, row.SKUID, row.ID)
+					);
+				}
+			});
+		});
+	}
 
-    getItemById(id) {
-        return new Promise((resolve, reject) => {
-            const sql = "SELECT * FROM ITEM_TABLE WHERE ID = ?";
-            this.db.get(sql, [id], (err, row) => {
-                if(err){
-                    reject(err);
-                    return;
-                }
-                if(row === undefined){
-                    resolve(undefined);
-                }else{
-                    resolve(new Item(row.DESCRIPTION, row.PRICE, row.USERID, row.SKUID, row.ID));
-                }
-            });
-        });
-    }
+	getItemBySupplierIdAndSKUId(supplierId, SKUId) {
+		return new Promise((resolve, reject) => {
+			const sql = 'SELECT * FROM ITEM_TABLE WHERE USERID = ? AND SKUID = ?';
+			this.db.get(sql, [supplierId, SKUId], (err, row) => {
+				if (err) {
+					reject(err);
+					return;
+				}
+				if (row === undefined) {
+					resolve(undefined);
+				} else {
+					resolve(
+						new Item(row.DESCRIPTION, row.PRICE, row.USERID, row.SKUID, row.ID)
+					);
+				}
+			});
+		});
+	}
 
-    getItemBySupplierIdAndSKUId(supplierId, SKUId) {
-        return new Promise((resolve, reject) => {
-            const sql = "SELECT * FROM ITEM_TABLE WHERE USERID = ? AND SKUID = ?";
-            this.db.get(sql, [supplierId, SKUId], (err, row) => {
-                if(err){
-                    reject(err);
-                    return;
-                }
-                if(row === undefined){
-                    resolve(undefined);
-                }else{
-                    resolve(new Item(row.DESCRIPTION, row.PRICE, row.USERID, row.SKUID, row.ID));
-                }
-            });
-        });
-    }
+	/*
+	getItemIDByProperties(description, price, userID, SKUID) {
+		return new Promise((resolve, reject) => {
+			const sql =
+				'SELECT ID FROM ITEM_TABLE ' +
+				'WHERE DESCRIPTION = ? AND PRICE = ? AND USERID = ? AND SKUID = ?';
+			this.db.get(sql, [description, price, userID, SKUID], (err, row) => {
+				if (err) {
+					reject(err);
+					return;
+				}
+				if (row === undefined) {
+					resolve(undefined);
+					return;
+				}
+				resolve(row.ID);
+			});
+		});
+	}
+    */
+	getAllItems() {
+		return new Promise((resolve, reject) => {
+			const sql = 'SELECT * FROM ITEM_TABLE';
+			this.db.all(sql, [], (err, rows) => {
+				if (err) {
+					reject(500);
+					return;
+				}
+				const items = rows.map((row) => {
+					return new Item(
+						row.DESCRIPTION,
+						row.PRICE,
+						row.USERID,
+						row.SKUID,
+						row.ID
+					);
+				});
+				resolve(items);
+			});
+		});
+	}
 
-    getItemIDByProperties(description, price, userID, SKUID){
-        return new Promise((resolve, reject) => {
-            const sql = "SELECT ID FROM ITEM_TABLE " +
-                        "WHERE DESCRIPTION = ? AND PRICE = ? AND USERID = ? AND SKUID = ?";
-            this.db.get(sql, [description, price, userID, SKUID], (err, row) => {
-                if(err){
-                    reject(err);
-                    return;
-                }
-                if(row === undefined){
-                    resolve(undefined);
-                    return;
-                }
-                resolve(row.ID);
-            });
-        });
-    }
+	editItem(id, newDescription, newPrice) {
+		return new Promise((resolve, reject) => {
+			const sql =
+				'UPDATE ITEM_TABLE SET DESCRIPTION = ?, PRICE = ? WHERE ID = ?';
+			this.db.run(sql, [newDescription, newPrice, id], (err) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve('ok');
+				}
+			});
+		});
+	}
 
-    getAllItems(){
-        return new Promise((resolve, reject) => {
-            const sql = "SELECT * FROM ITEM_TABLE";
-            this.db.all(sql, [], (err, rows) => {
-                if(err){
-                    reject(500);
-                    return;
-                }
-                const items = rows.map(row => {
-                    return new Item(row.DESCRIPTION, row.PRICE, row.USERID, row.SKUID, row.ID);
-                });
-                resolve(items);
-            });
-        });
-    }
-
-    editItem(id, newDescription, newPrice){
-        return new Promise((resolve, reject) => {
-            const sql = "UPDATE ITEM_TABLE SET DESCRIPTION = ?, PRICE = ? WHERE ID = ?";
-            this.db.run(sql, [newDescription, newPrice, id], err => {
-                if(err){
-                    reject(err);
-                }else{
-                    resolve('ok');
-                }
-            });
-        });
-    }
-
-    deleteItem(id){
-        return new Promise((resolve, reject) => {
-            const sql = "DELETE FROM ITEM_TABLE WHERE ID = ?";
-            this.db.run(sql, [id], err => {
-                if(err){
-                    reject(err);
-                }else{
-                    resolve('ok');
-                }
-            });
-        });
-    }
+	deleteItem(id) {
+		return new Promise((resolve, reject) => {
+			const sql = 'DELETE FROM ITEM_TABLE WHERE ID = ?';
+			this.db.run(sql, [id], (err) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve('ok');
+				}
+			});
+		});
+	}
 }
 
 module.exports = ItemDAO;
